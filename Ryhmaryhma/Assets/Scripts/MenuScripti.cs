a5df94d186a358b55e38aff39045734eb1b230c4
fEISTII <63512941+fEISTII@users.noreply.github.com>
2023-12-07 14:59:23 +0200

    main menua aloitetttu

---
+++ Ryhmaryhma/Assets/Scripts/MenuScripti.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScripti : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartBtnClicked()
+    {
+        SceneManager.LoadScene("Taso1");
+    }
+    public void TasotBtnClicked()
+    {
+        SceneManager.LoadScene("TasoValikko");
+    }
+
+    public void HuippuajatClicked()
+    {
+        SceneManager.LoadScene("Huippuajat");
+    }
+
+    public void EndBtnClicked()
+    {
+        Application.Quit();
+    }
+}

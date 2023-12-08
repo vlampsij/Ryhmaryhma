4feb244f8c44d54c519f5b1715847f520f765efc
fEISTII <63512941+fEISTII@users.noreply.github.com>
2023-12-08 10:49:56 +0200

    ajastin lisätty

---
+++ Ryhmaryhma/Assets/Scripts/Ajastin.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Threading;
+
+public class Ajastin : MonoBehaviour {
+
+    private float ajanKesto = 3f * 60f;
+
+    [SerializeField]
+    private bool ajanLasku = true;
+
+    private float ajastin;
+
+    [SerializeField]
+    private TextMeshProUGUI ekaMinuutti;
+    [SerializeField]
+    private TextMeshProUGUI tokaMinuutti;
+    [SerializeField]
+    private TextMeshProUGUI jakaja;
+    [SerializeField]
+    private TextMeshProUGUI ekaSekunti;
+    [SerializeField]
+    private TextMeshProUGUI tokaSekunti;
+
+    private float flashAjastin;
+    private float flashKesto;
+
+    void Start()
+    {
+        ResetTimer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ajanLasku && ajastin > 0)
+        {
+            ajastin -= Time.deltaTime;
+            UpdateTimerDisplay(ajastin);
+        } else if (!ajanLasku && ajastin < ajanKesto)
+        {
+            ajastin += Time.deltaTime;
+            UpdateTimerDisplay(ajastin);
+        }
+        else
+        {
+            Flash();
+        }
+    }
+    private void ResetTimer()
+    {
+        if(ajanLasku)
+        {
+            ajastin = ajanKesto;
+        } else
+        {
+            ajastin = 0;
+        }
+        SetTextDisplay(true);
+    }
+    private void UpdateTimerDisplay(float time)
+    {
+        float minuutit = Mathf.FloorToInt(time / 60);
+        float sekunnit = Mathf.FloorToInt(time % 60);
+
+        string tamaAika = string.Format("{00:00}{1:00}", minuutit, sekunnit);
+        ekaMinuutti.text = tamaAika[0].ToString();
+        tokaMinuutti.text = tamaAika[1].ToString();
+        ekaSekunti.text = tamaAika[2].ToString();
+        tokaSekunti.text = tamaAika[3].ToString();
+    }
+    private void Flash()
+    {
+        if(ajanLasku && ajastin != 0) 
+        {
+            ajastin = 0;
+            UpdateTimerDisplay(ajastin);
+        }
+        if(!ajanLasku && ajastin != ajanKesto)
+        {
+            ajastin = 0;
+            UpdateTimerDisplay(ajastin);
+        }
+        if(flashAjastin <= 0)
+        {
+            flashAjastin = flashKesto;
+        } else if (flashAjastin >= flashKesto / 2)
+        {
+            flashAjastin -= Time.deltaTime;
+            SetTextDisplay(true);
+        }
+    }
+    private void SetTextDisplay(bool enabled)
+    {
+        ekaMinuutti.enabled = enabled;
+        tokaMinuutti.enabled = enabled;
+        ekaSekunti.enabled = enabled;
+        tokaSekunti.enabled = enabled;
+        jakaja.enabled = enabled;
+
+    }
+}

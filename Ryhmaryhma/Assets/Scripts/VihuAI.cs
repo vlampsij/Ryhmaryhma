687ff216282f312d6ad67c17c2f9054931c81a65
vlampsij <veera.lampsijarvi@edu.lapinamk.fi>
2023-12-11 23:39:38 +0200

    fix

--- Ryhmaryhma/Assets/Scripts/VihuAI.cs
+++ Ryhmaryhma/Assets/Scripts/VihuAI.cs
@@ -28,7 +28,7 @@ public class VihuAI : MonoBehaviour
     private void Awake()
     {
         //Ottaa pelaajan muuttujaan
-        pelaaja = GameObject.Find("Astronaut").transform;
+        pelaaja = GameObject.Find("PlayerPlacehold").transform;
         agent = GetComponent<NavMeshAgent>();
         animaatio = GetComponentInChildren<Animator>();
     }
